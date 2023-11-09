e4aa3af46dd79efbbd3bcce410875eb5aa81cf94
Jōshin <git@wholezero.org>
2023-11-09 14:17:32 -0500

    sort devDependencies, add yarn/bun i calls

--- broken.sh
+++ broken.sh
@@ -6,6 +6,7 @@ export NODE_ENV=production
 export NEXTAUTH_URL=http://localhost:3000
 export NEXTAUTH_SECRET=1
 
+bun i
 bun run build
 cp -a .next/static .next/standalone/.next/static
 cp -a .env.local .next/standalone
--- works.sh
+++ works.sh
@@ -6,6 +6,7 @@ export NODE_ENV=production
 export NEXTAUTH_URL=http://localhost:3000
 export NEXTAUTH_SECRET=1
 
+yarn
 yarn build
 cp -a .next/static .next/standalone/.next/static
 cp -a .env.local .next/standalone
