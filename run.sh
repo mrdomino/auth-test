43968c6d368da8cb7c3657451813c365a93f7709
Jōshin <git@wholezero.org>
2023-11-09 14:50:32 -0500

    error outside of standalone

--- run.sh
+++ run.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
-# Works:
-#     ./run.sh node
-#
-# Broken:
-#     ./run.sh bun
 set -eux
+
+case $1 in
+  *good*)
+    SERVE='bun'
+    shift
+    ;;
+  *)
+    SERVE='bun --bun'
+    shift
+    ;;
+esac
 
 export NODE_ENV=production
 export NEXTAUTH_URL=http://localhost:3000
 export NEXTAUTH_SECRET=1
 
-rm -rf .next
 bun i
 bun run build
-cp -a .next/static .next/standalone/.next/static
-cp .env.local .next/standalone
-exec $1 .next/standalone/server.js
+exec $SERVE start
