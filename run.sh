eb94710491e17924b2a44404e2e3245316e9c709
Jōshin <git@wholezero.org>
2023-11-09 14:25:18 -0500

    modularize run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-
 set -eux
 
 export NODE_ENV=production
 export NEXTAUTH_URL=http://localhost:3000
 export NEXTAUTH_SECRET=1
 
-rm -rf .next
-yarn
-yarn build
-cp -a .next/static .next/standalone/.next/static
-cp -a .env.local .next/standalone
+source $1.env
 
-exec node .next/standalone/server.js
+rm -rf .next
+$PKG
+$BUILD
+cp -a .next/static .next/standalone/.next/static
+cp .env.local .next/standalone
+exec $SERVE .next/standalone/server.js
